a270d38989fe5137908b551728ca3f60fd5ce965
Sukhbirsingh Khalsa <sukhbirkhalsait@gmail.com>
2023-11-14 16:20:06 +0530

    Added Load, Retrieve, Delete record implmentations

--- suitescript1.0/Record APIs.js
+++ suitescript1.0/Record APIs.js
@@ -1,4 +1,4 @@
-// Create a record with initializeValues, body level fields, line level fields
+//---------------------- Create a record with initializeValues, body level fields, line level fields
 bodyLevelFields = {
     location: "14",
     orderstatus: "B",
@@ -41,4 +41,44 @@ function createNSRecord(type, bodyLevelFields, lineItems, initializeValues) {
     id = nlapiSubmitRecord(record, true);
     return id;
 }
-createNSRecord('salesorder', bodyLevelFields, lineItems, initializeValues);
+// createNSRecord('salesorder', bodyLevelFields, lineItems, initializeValues);
+
+
+//-------------------------------------Load an Exisiting Record
+nlapiLoadRecord(type, id, initializeValues)
+var record = nlapiLoadRecord('invoice', 51729);
+
+
+//-------------------------------------Get Body Level Fields from  Exisiting Record
+
+// getFieldValues(fldnam) - return the value of given fieldid
+record.getFieldValue('email')
+
+// record.getFieldText(fldnam) - return the value on the UI 
+record.getFieldText('location')
+
+
+//-------------------------------------Get Line Level Fields from  Exisiting Record
+
+//recordObject.getLineItemText(sublisttype, fieldid, linenumber) --return value on the UI
+record.getLineItemText('item', 'price', 1)
+
+//recordObject.getLineItemValue(sublisttype, fieldid, linenumber) --return internal id or value depending on the field
+//for example - fieldid = item will return internal id, while amount will give the amount value of given line item
+record.getLineItemValue('item', 'amount', 1)
+
+record.getLineItemCount('item')
+
+
+//-------------------------------------Delete Exisiting Record from NS - no undo
+nlapiDeleteRecord(type, id, initializeValues)
+var id  = nlapiDeleteRecord('salesorder', '51729') //return an ID of deleted record type, it permantely deletes it from netsuite
+
+
+// --------------------------------------------Get Record Id, Type, Current logged in role,
+nlapiGetRecordId() // --get current record id in the given context
+nlapiGetRecordType() //-- get current record type in the given context
+nlapiGetRole() // to get the current logged in role internal id
+
+
+
